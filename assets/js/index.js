a859c5d18fd0190b5c6a9c91f8b15cfd4aa70c7d
david-commit <david.ondiege@students.moringaschool.com>
2022-07-15 23:22:55 +0300

    Added is selectors to HTML elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,30 @@
-function getSelectValue() {
-    var selectedValue = document.getElementById("selectShed").value
-    console.log(selectedValue)
+// DROPDOWN OPTIONS
+// function getSelectValue() {
+//     var selectedValue = document.getElementById("optionShedA").value
+//     console.log(selectedValue)
+// }
+
+document.getElementById("addBtn").onclick = function(){
+
+let quantityInputA = document.getElementById("quantityInputA").value;
+console.log(quantityInputA);
+
+let quantityInputB = document.getElementById("quantityInputB").value;
+console.log(quantityInputB);
+
+let quantityInputC = document.getElementById("quantityInputC").value;
+console.log(quantityInputC);
+
+let quantityInputD = document.getElementById("quantityInputD").value;
+console.log(quantityInputD);
+
 }
 
-document.getElementById
+
+
+
+
+
 
 
 
@@ -26,7 +47,6 @@ document.getElementById
 // }
 
 // namePrompt()
-
 
 
 
