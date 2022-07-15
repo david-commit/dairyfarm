a9268f0c72fb422524b84e06d47c504df5a91bae
david-commit <david.ondiege@students.moringaschool.com>
2022-07-15 21:03:38 +0300

    Major change from Options toform Item Labels

---
+++ index.js
@@ -0,0 +1,41 @@
+function getSelectValue() {
+    var selectedValue = document.getElementById("selectShed").value
+    console.log(selectedValue)
+}
+
+document.getElementById
+
+
+
+
+
+
+
+// var shed = {"shedA" , "valueShedA"
+//     "shedD" , "valueShedB"
+// }
+
+
+// let nm = document.getElementById("name")
+
+// function namePrompt(){
+//     let nam = prompt("What's your name")
+
+//     nm.innerHTML = name
+
+// }
+
+// namePrompt()
+
+
+
+
+// const color = document.getElementById("color")
+
+// function colorPrompt(){
+//     let color = 
+
+//     color.innerHTML = color
+// }
+
+// colorPrompt()
